1dfd12986cacfc4728bf86ed54bfae11722dd1b6
Thamarai <tmanalan710@gmail.com>
2024-09-26 21:53:48 +0530

    Trying to add the addTodo function

--- front-end/index.js
+++ front-end/index.js
@@ -1,3 +1,20 @@
-function display(){
-    alert("add the todo")
+function addTodo(){
+
+    const newTodo = document.getElementById("todo-input").value.trim();
+
+    const li = document.createElement('li');
+    li.className="border py-2 px-4 basis-1/3 mr-2 rounded";
+
+    const newTodoText = document.createTextNode(newTodo);
+
+    li.appendChild(newTodoText);
+
+    document.getElementById("todo-main-list").appendChild(li);
+    
+}
+function editTodo(){
+
+}
+function deleteTodo(){
+
 }
